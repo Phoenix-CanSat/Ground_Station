6bb4fde3a1181f3692551b44e6640d55fd45b471
Dimitris Dafermos <dimitrisjim321@gmail.com>
2022-04-21 22:32:47 +0300

    Added fake data option for debugging without Bob.

--- src/RF.cpp
+++ src/RF.cpp
@@ -5,6 +5,10 @@
 #include "RF.h"
 #include "Output.h"
 
+#define FAKEDATA true
+
+int8_t fakeData = 0;
+
 #define RFM_CS 53
 #define RFM_RST 2
 #define RFM_INT 3
@@ -46,6 +50,16 @@ bool RFInit() {
 // Checks if data from Bob is received.
 bool RFReceiveData(char data[]) {
     
+    if (FAKEDATA) {
+        if (fakeData == 5) {
+            snprintf(data, 225, "PHX,1,1000,22.53,1014.48,3808.5532,2400.8757,-0.19,35.07,30.00,-56,22.64,1014.88");
+            return true;
+        } else {
+            fakeData += 1;
+            return false;
+        }
+    }
+
     if (rf_init) {
 
         uint8_t packet[225];
